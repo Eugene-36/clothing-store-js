59e728bd2d35d2495f7382fc35d939fe0dcc6893
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-01-05 22:47:40 +0200

    responsive work

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const bar = document.getElementById('bar');
+const closeBar = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
+console.log(nav);
+if (bar) {
+  bar.addEventListener('click', (e) => {
+    nav.classList.add('active');
+  });
+}
+
+if (closeBar) {
+  closeBar.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
