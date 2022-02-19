218742d68d3243ff6e2147d137e10f65e3d8fa95
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-02-19 10:34:43 +0200

    added smooth scroll

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ if (closeBar) {
     nav.classList.remove('active');
   });
 }
+
+//* ===================SHOW SCROLL UP===================================
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
+  console.log('scrollUp', scrollUp);
+}
+window.addEventListener('scroll', scrollUp);
+
+//*=======================SCROLL======================
+document.querySelector('.active').onclick = () => {
+  window.scrollTo(pageYOffset, 0);
+};
